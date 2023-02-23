5911d92089f5b4251b5e711ddc931c9e1a6b3a40
Alan Rios <35049559+hirios@users.noreply.github.com>
2023-02-23 15:38:50 -0300

    Update awaiting_validation.js

--- awaiting_validation.js
+++ awaiting_validation.js
@@ -5,6 +5,6 @@ function saveDraft() {
     document.execCommand('insertText', false, ' ')
 }
 
-var g_my_note = `'<p><strong>Date:</strong>&nbsp;01/03/2023</p>\n<p><strong>Speakeasy ID:</strong></p>\n<p><strong>Substatus:</strong> NI - Awaiting Validation</p>\n<p><strong>Reason/Comments</strong>: Need to check for new conversions</p>\n<p><strong>On Call:</strong> Configuramos as conversões otimizadas na conversão de envio de formulário. O procedimento foi feito utilizando o GTM.</p>\n<p dir="auto"><strong>Screenshots:</strong></p>'`
+var g_my_note = `<p><strong>Date:</strong>&nbsp;01/03/2023</p>\n<p><strong>Speakeasy ID:</strong></p>\n<p><strong>Substatus:</strong> NI - Awaiting Validation</p>\n<p><strong>Reason/Comments</strong>: Need to check for new conversions</p>\n<p><strong>On Call:</strong> Configuramos as conversões otimizadas na conversão de envio de formulário. O procedimento foi feito utilizando o GTM.</p>\n<p dir="auto"><strong>Screenshots:</strong></p>`
 document.querySelector('[aria-label="Case Note"]').innerHTML = g_my_note
 saveDraft()
