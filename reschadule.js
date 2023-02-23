09c6b49e8756b9860c28d5f15e2422f21e882e28
Alan Rios <35049559+hirios@users.noreply.github.com>
2023-02-23 15:40:13 -0300

    Update reschadule.js

--- reschadule.js
+++ reschadule.js
@@ -5,6 +5,6 @@ function saveDraft() {
     document.execCommand('insertText', false, ' ')
 }
 
-var g_my_note = `'<p><strong>Date:</strong>&nbsp;01/03/2023</p>\n<p><strong>Speakeasy:</strong></p>\n<p><strong>Substatus:</strong>&nbsp;AS - Reschedule 1</p>\n<p><strong>Reason/Comments:</strong>&nbsp;Customer requested rescheduling</p>\n<p><strong>On call:</strong>&nbsp;Cliente solicitou o reagendamento da consultoria</p>\n<p dir="auto"><strong>Screenshot:</strong></p><p dir="auto"><strong><br></strong></p>'`
+var g_my_note = `<p><strong>Date:</strong>&nbsp;01/03/2023</p>\n<p><strong>Speakeasy:</strong></p>\n<p><strong>Substatus:</strong>&nbsp;AS - Reschedule 1</p>\n<p><strong>Reason/Comments:</strong>&nbsp;Customer requested rescheduling</p>\n<p><strong>On call:</strong>&nbsp;Cliente solicitou o reagendamento da consultoria</p>\n<p dir="auto"><strong>Screenshot:</strong></p><p dir="auto"><strong><br></strong></p>`
 document.querySelector('[aria-label="Case Note"]').innerHTML = g_my_note
 saveDraft()
