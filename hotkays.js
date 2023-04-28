ab4849be873e1292899d99c2945126b4e8c78b24
Alan Rios <35049559+hirios@users.noreply.github.com>
2023-04-28 12:00:47 -0300

    Update hotkays.js

--- hotkays.js
+++ hotkays.js
@@ -2,7 +2,7 @@ const verboseCase = true
 
 function print(string) {
     if (verboseCase) {
-        console.log(string)
+        console.log('%c ' + string, 'background: #185244; color: #ffffff; font-weight:bold;');
     }
 }
 
@@ -88,58 +88,6 @@ async function sendTemplate(hotKay) {
                 })
 
             }).then(async () => {
-                console.log('VEIO ATÉ AQUI')
-                await waitForElemente('dynamic-component[class*="dynamic-item"]:nth-child(1)').then(async (matchCR) => {
-                    matchCR.click()
-                })
-            })
-        })
-    })
-
-}
-
-setLanguage('Portuguese (Brazil)').then(() => {
-    sendTemplate('ts as resched1')
-}))
-console.log('DEU CERTO')
-          })
-    }
-  })
-}
-
-async function sendTemplate(hotKay) {
-    let menu = document.querySelector('[role="menu"]') || document.querySelector('[aria-label="Create a write card"]')
-    menu.dispatchEvent(new Event('focus', { 'bubbles': true }))
-
-    // Aguara o botão de criar email aparecer e faz o clique
-    await waitForElemente('[aria-label="Create new email"]').then(async (botaoEmail) => {
-        botaoEmail.click()
-
-        // Remove trechos do email padrão
-    }).then(async () => {
-        await waitForElemente('#email-body-content').then(async () => {
-            await sleep(500)
-            const email = document.querySelectorAll('#email-body-content');
-            email[email.length - 1].innerText = '';
-        })
-
-        // Depois de clicado aguarda o botão do CR aparecer e clica nele
-    }).then(async () => {
-        await waitForElemente('[aria-label="Insert canned response"][role="button"][animated="true"][aria-disabled="false"]').then(async (botaoCR) => {
-            botaoCR.click()
-
-        }).then(async () => {
-            await waitForElemente('search-panel[debug-id="search-panel"] input').then(async (searchElement) => {
-                searchElement.click()
-
-            }).then(async () => {
-                await waitForElemente('search-panel input[aria-owns]').then(async (searchInput) => {
-                    searchInput.value = hotKay
-                    document.execCommand('insertText', false, ' ')
-                })
-
-            }).then(async () => {
-                console.log('VEIO ATÉ AQUI')
                 await waitForElemente('dynamic-component[class*="dynamic-item"]:nth-child(1)').then(async (matchCR) => {
                     matchCR.click()
                 })
