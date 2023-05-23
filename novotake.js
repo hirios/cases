d163c5c05ff1a9ded48702b810a70eb8475b6901
Alan Rios <35049559+hirios@users.noreply.github.com>
2023-05-23 17:56:12 -0300

    Create novotake.js

---
+++ novotake.js
@@ -0,0 +1,152 @@
+async function waitForElemente(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        }
+        );
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+            attributes: true
+        });
+    }
+    );
+}
+
+async function waitChanged(target, index = 0) {
+    return new Promise(resolve => {
+        // let target = document.querySelectorAll(selector)[index];
+        try {
+            target.querySelector('ng-template').click()
+        } catch (e) {}
+        
+        let observer = new MutationObserver(function (mutations) {
+            if (target.innerText.includes('*')) {
+                return observer.observe(target, config);
+            } else {
+                console.log('FIELD DESOCULTADO')
+                resolve(target.innerText)
+                observer.disconnect();
+            }
+        });
+        let config = { childList: true };
+        observer.observe(target, config);
+    })
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function g_Appointment() {
+    let dateTime = Array.from(document.querySelectorAll('cuf-form-field')).filter(function (e) { return e.innerText.includes('Time') })[0].querySelector('[debug-id="html-value"]').innerText
+    dateTime = new Date(dateTime)
+    let date = dateTime.toLocaleDateString()
+    let time = dateTime.toLocaleTimeString().split(':').slice(0, 2).join(':')
+    return `${date} às ${time}`
+}
+
+function g_getTasks() {
+    let tasksElement = Array.from(document.querySelectorAll('cuf-form-field')).filter(function (e) { return e.innerText.includes('Tasks') })[0]
+    let allTasks = Array.from(tasksElement.querySelectorAll('[debug-id="html-value"]')).map(x => x.innerText)
+    return allTasks.join(', ')
+}
+
+function setEmails(email1, email2) {
+    console.log('EMAILLLLLLLL')
+
+    var email1 = email1
+    var email2 = email2
+
+    var emailsFields = document.querySelectorAll('email-address-input')
+    var emailTo = emailsFields[0].querySelector('input')
+    var emailCC = emailsFields[1].querySelector('input')
+
+    if (!emailsFields[0].innerText.includes('@')) {
+        emailTo.focus()
+        emailTo.value = email1
+        document.execCommand('insertText', false, ' ')
+    }
+
+
+    if (!emailsFields[1].innerText.includes('@')) {
+        emailCC.focus()
+        emailCC.value = email2
+        document.execCommand('insertText', false, ' ')
+    }
+}
+
+function myCase() {
+    return Array.from(document.querySelectorAll('[id="email-body-content-top-content"]')).pop()
+}
+
+
+
+// CLICA NO OVERVIEW
+document.querySelector('[aria-controls="read-card-tab-panel-home"]').click()
+
+var g_accountStrategist = ''
+var g_clientEmail = ''
+var g_appointment = '<span class="field">{HORARIO}</span>'
+var g_tasks = '<span class="field">{TASKS}</span>'
+var g_caseID = '<span class="field">{WEBSITE}</span>'
+var g_clientName = '<span class="field">{NOME DO CLIENTE}</span>'
+var g_website = '<span class="field">{SITE}</span>'
+var g_clientPhone = '<span class="field">{TELEFONE}</span>'
+
+var g_appointment = g_Appointment()
+var g_tasks = g_getTasks()
+var g_caseID = document.querySelector('[src="https://pulse-tracker.corp.google.com/tracking_script.js"]').getAttribute('data-case-id')
+var g_clientName = document.querySelector('title').innerText.split(' ')[1]
+var g_website = document.querySelector('ng-template > [href*="https://www.google.com/url?q="]').innerText
+var g_accountStrategist = document.querySelector('internal-user-info div div[class*="email"]').innerText
+
+// AGUARDA O OVERVIEW SER SELECIONADO
+waitForElemente('[aria-label="Overview"].selected').then(() => {
+    let g_pii_values = document.querySelectorAll('cuf-form-field pii-value')
+
+    if (g_pii_values.length > 0 && g_pii_values[0].innerText) {
+        g_emailElement = g_pii_values[0].innerText.includes('@') ? g_pii_values[0] : undefined
+        waitChanged(g_emailElement).then((info) => {
+            g_clientEmail = info
+        })
+    }
+
+    if (g_pii_values.length > 1 && g_pii_values[1].innerText) {
+        g_phoneElement = g_pii_values[1].innerText.includes('+') ? g_pii_values[1] : undefined
+        waitChanged(g_phoneElement).then((info) => {
+            g_clientPhone = info.replace(' ', '')
+        })
+    }
+})
+
+async function take() {
+    let menu = document.querySelector('[role="menu"]') || document.querySelector('[aria-label="Create a write card"]')
+    menu.dispatchEvent(new Event('focus', { 'bubbles': true }))
+
+    // Aguarda o botão de criar email aparecer e faz o clique
+    await waitForElemente('[aria-label="Create new email"]').then(async (botaoEmail) => {
+        botaoEmail.click()
+
+    }).then(async () => {
+        await waitForElemente('email-address-input').then(async () => {
+            console.log('SETANDO OS EMAILS')
+            sleep(500)
+            setEmails(g_clientEmail, g_accountStrategist)
+        }).then(() => {
+            console.log('TENTANDO ALTERAR O EMAIL')
+            myCase().innerHTML = `<p dir="auto"><em>Tenha em atenção que, de acordo com a Política de Privacidade da Google, todas as capturas de ecrã e links abaixo/acima são apenas para uso interno. Não as partilhe/encaminhe externamente.</em></p> <div id="email-body-content-top-content" style="font:normal 13px/17px Roboto,sans-serif;display:block" dir="auto"> <p dir="ltr"><br></p> <p dir="auto">Oi <span style="font-weight:bold"><span class=fi">${g_clientName}</span></span>,</p> <p dir="auto">Muito obrigado por agendar uma consultoria com o Time de Soluções Técnicas do Google. O seu Representante do Google solicitou esta chamada em seu nome e te auxiliaremos no caso <span style="font-weight:bold"><span class=fi">${g_caseID}</span></span> para o website <span style="font-weight:bold"><span class=fi">${g_website}</span></span>.</p> <p dir="auto">O agente do time de Soluções Técnicas irá entrar em contato na data/horário agendado: <span style="font-weight:bold"><span class=fi">${g_appointment}</span></span>.&nbsp;</p> <p dir="auto">Por favor, verifique a sua caixa de entrada e acesse o convite no seu calendário. Siga as instruções específicas nele e confirme a sua participação. Para se preparar para a nossa chamada, por favor, revise a lista de tarefas <a href="https://support.google.com/google-ads/answer/11605860?hl=pt-BR" style="color:rgb(26,115,232)" target="_blank">vinculada aqui </a>e complete as solicitações.</p> <p dir="ltr">Se tiver qualquer dúvida antes da chamada ou se quiser adicionar outros participantes à reunião, basta nos informar respondendo a esta mensagem ou entrando em contato com o Representante do Google. Estamos animados para trabalhar com você!</p><br> <p dir="auto">Muito obrigado!</p> <div dir="ltr" align="left"> <table style="border:none;border-collapse:collapse;line-height:normal"> <colgroup> <col width="361"> </colgroup> <tbody> <tr style="height:151.75pt"> <td style="vertical-align:top;padding:5pt 5pt 5pt 5pt;overflow:hidden"><br> <div dir="ltr" style="margin-left:0pt" align="left"> <table style="border:none;border-collapse:collapse"> <colgroup> <col width="82"> <col width="197"> </colgroup> <tbody> <tr style="height:67.5pt"> <td style="border-right:solid #d5d5d5 0.75pt;vertical-align:middle;padding:0pt 15pt 0pt 0pt;overflow:hidden"> <p dir="ltr" style="line-height:1.656;margin-top:0pt;margin-bottom:0pt"> <span style="font-size:12pt;font-family:Arial;color:rgb(0,0,0);vertical-align:baseline;white-space:pre-wrap"><span style="border:none;display:inline-block;overflow:hidden;width:59px;height:59px"><img src="https://lh3.googleusercontent.com/y6QudS2DJDVsUlt-v6ncpiUKx9DbLrX9Y9kSCmhcnnHjMIFSLjXgX2l5KCP9TZ89o3TM22bi6kyNi6d7lVWEyqvMfsgEDS3Al2O9xpWKStOCy1Li5y-Ht58_1T8iX7dy5uUBvAQgf19njRB27eBGEIuPuv9hSkEKupeEe8vxhAt9yddmXrconSRRgvGxgVo" width="59" height="59" style="margin-left:0px;margin-top:0px"></span></span></p> </td> <td style="border-left:solid #d5d5d5 0.75pt;vertical-align:top;padding:0pt 0pt 0pt 15pt;overflow:hidden"> <br> <div dir="ltr" style="margin-left:0pt" align="left"> <table style="border:none;border-collapse:collapse"> <colgroup> <col width="138"> </colgroup> <tbody> <tr style="height:40.5pt"> <td colspan="2" style="vertical-align:top;padding:0pt 0pt 4pt 0pt;overflow:hidden"> <p dir="ltr" style="line-height:1.656;margin-top:0pt;margin-bottom:0pt"> <span style="font-size:9pt;font-family:&quot;Google Sans&quot;,sans-serif;color:rgb(56,58,53);vertical-align:baseline;white-space:pre-wrap">Equipe de Soluções Técnicas do Google</span></p> </td> </tr> </tbody> </table> </div><br> </td> </tr> </tbody> </table> </div><br> <p dir="auto" style="line-height:1.656;margin-top:0pt;margin-bottom:0pt"><span style="font-size:8pt;color:rgb(0,0,0);font-style:italic;vertical-align:baseline;white-space:pre-wrap">Não remova o código de referência para que possamos responder mais rapidamente. Obrigado.</span></p><br> </td> </tr> </tbody> </table> </div> <p dir="ltr"><span style="font-weight:bold;font-style:italic">Termos</span><span style="font-style:italic">: ao responder a esta mensagem, você autoriza o especialista em implementação do Google a fornecer orientações quanto à instalação do Offline Conversion Tracking no seu site e/ou campanha e/ou conta do Google Analytics, conforme necessário. Você confirma e reconhece que todas as alterações sugeridas e/ou implementadas pelo Google devem ser realizadas por sua conta e risco. O Google não garante nem promete resultados decorrentes da implementação dessas alterações. Além disso, o Google não será responsável por eventuais efeitos dessas mudanças, o que inclui, sem limitação, qualquer aumento nos gastos relacionados às campanhas do Google Ads, pelos quais você terá total responsabilidade. Você também concorda em validar se o site continua funcionando conforme o esperado.</span></p><br> <p dir="auto"><span style="font-weight:bold;font-style:italic">Observação</span><span style="font-style:italic">: recomendamos que você faça o backup completo do seu site antes de realizar alterações. Além disso, você NÃO deve compartilhar as credenciais do site comigo ou com outras pessoas do Google. Por fim, fazer alterações no seu website / conta enquanto trabalhamos na solicitação pode causar a perda de algumas alterações / configurações. Aconselhamos que você não faça alterações no seu site / conta até a conclusão das implementações recomendadas.</span></p> </div>`
+        })
+    })
+}
+
+sleep(1)
+take()
