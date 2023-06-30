7ff58ae12a6073a5e21d7b8ddb711bb3eefc71d3
Alan Rios <35049559+hirios@users.noreply.github.com>
2023-06-30 14:05:19 -0300

    Update novotake.js

--- novotake.js
+++ novotake.js
@@ -73,43 +73,42 @@ async function setLanguage(language) {
 }
 
 async function sendTemplate(hotKay) {
-  let menu = document.querySelector('[role="menu"]') || document.querySelector('[aria-label="Create a write card"]')
-  menu.dispatchEvent(new Event('focus', { 'bubbles': true }))
+    let menu = document.querySelector('[aria-label="Create a write card"]')
+    menu.dispatchEvent(new Event('focus', { 'bubbles': true }))
 
-  // Aguara o botão de criar email aparecer e faz o clique
-  await waitForElemente('[aria-label="Create new email"]').then(async (botaoEmail) => {
-    botaoEmail.click()
+    // Aguara o botão de criar email aparecer e faz o clique
+    await waitForElemente('[aria-label="Create new email"]').then(async (botaoEmail) => {
+        botaoEmail.click()
 
-    // Remove trechos do email padrão
-  }).then(async () => {
-    await waitForElemente('compose-card-content-wrapper #email-body-content').then(async (email) => {
-      await technicalSolutions()
-      await sleep(500)
-      email.innerText = '';
-    })
-
-    // Depois de clicado aguarda o botão do CR aparecer e clica nele
-  }).then(async () => {
-    await waitForElemente('[aria-label="Insert canned response"][role="button"][animated="true"][aria-disabled="false"]').then(async (botaoCR) => {
-      botaoCR.click()
-
+        // Remove trechos do email padrão
     }).then(async () => {
-      await waitForElemente('search-panel[debug-id="search-panel"] input').then(async (searchElement) => {
-        searchElement.click()
-
-      }).then(async () => {
-        await waitForElemente('search-panel input[aria-owns]').then(async (searchInput) => {
-          searchInput.value = hotKay
-          document.execCommand('insertText', false, ' ')
+        await waitForElemente('compose-card-content-wrapper #email-body-content').then(async (email) => {
+            await sleep(200)
+            email.innerText = '';
         })
 
-      }).then(async () => {
-        await waitForElemente('dynamic-component[class*="dynamic-item"]:nth-child(1)').then(async (matchCR) => {
-          matchCR.click()
+        // Depois de clicado aguarda o botão do CR aparecer e clica nele
+    }).then(async () => {
+        await waitForElemente('[aria-label="Insert canned response"][role="button"][animated="true"][aria-disabled="false"]').then(async (botaoCR) => {
+            botaoCR.click()
+
+        }).then(async () => {
+            await waitForElemente('search-panel[debug-id="search-panel"] input').then(async (searchElement) => {
+                searchElement.click()
+
+            }).then(async () => {
+                await waitForElemente('search-panel input[aria-owns]').then(async (searchInput) => {
+                    searchInput.value = hotKay
+                    document.execCommand('insertText', false, ' ')
+                })
+
+            }).then(async () => {
+                await waitForElemente('dynamic-component[class*="dynamic-item"]:nth-child(1)').then(async (matchCR) => {
+                    matchCR.click()
+                })
+            })
         })
-      })
     })
-  })
 
 }
 
